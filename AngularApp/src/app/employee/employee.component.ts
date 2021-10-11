cc967c5d246e3a563b8019ce8263a93503de704a
Saruf-Ratul <saruftoratul95@gmail.com>
2021-10-12 02:48:12 +0600

    edit

---
+++ AngularApp/src/app/employee/employee.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../shared/employee.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+  providers: [EmployeeService]
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(public employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+  }
+
+}
